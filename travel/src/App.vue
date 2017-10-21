f5851ad2df0fca980cf31d1fede01f61f0cf4174
林润茏 <lrl@xingyunbooks.com>
2017-10-21 16:49:41 +0800

    修改了底部导航的样式
    修改了底部导航的样式

--- travel/src/App.vue
+++ travel/src/App.vue
@@ -3,19 +3,37 @@
       <v-header v-bind:volunteer="volunteer"></v-header>
         <div class="tab">
           <div class="tab-item">
+
+            <router-link to="/volunteerPost">
               <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-lake__easyiconnet1"></use>
-             </svg>
-              <router-link to="/volunteerPost">招募帖</router-link>
+                <use xlink:href="#icon-road__easyiconnet"></use>
+              </svg>
+              <span class="icon-font">招募帖</span>
+            </router-link>
           </div>
           <div class="tab-item">
-              <router-link to="/diary">旅记</router-link>
+              <router-link to="/diary">
+                <svg class="icon" aria-hidden="true">
+                  <use xlink:href="#icon-iceberg__easyiconnet"></use>
+                </svg>
+                <span class="icon-font">旅记</span>
+              </router-link>
           </div>
           <div class="tab-item">
-              <router-link to="/voiceMap">声音地图</router-link>
+              <router-link to="/voiceMap">
+                <svg class="icon" aria-hidden="true">
+                  <use xlink:href="#icon-lake__easyiconnet1"></use>
+                </svg>
+                <span class="icon-font"> 声音地图</span>
+              </router-link>
           </div>
           <div class="tab-item">
-            <router-link to="/personal">我</router-link>
+            <router-link to="/personal">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-reindeer__easyiconnet"></use>
+              </svg>
+              <span class="icon-font"> 我</span>
+            </router-link>
           </div>
         </div>
         <router-view></router-view>
@@ -46,17 +64,36 @@
     position:fixed;
     bottom:0;
     display:flex
-    width:100%
-    height:40px;
-    line-height:40px
+    flex-direction :row;
+    padding-top :3px;
+    width:100%;
+    height:55px;
+    line-height:40px;
+    background-color:rgba(140,180,250,0.8);
+    font-family :FangSong;
+
     .tab-item
-      flex:1
+      flex:1;
       &>a
-        display: block;
+        display :flex;
+        flex-direction:column;//定义项目的对齐方向
+        align-items:center ;//定义项目在交叉轴上
         width :100%;
+        height :100px;
         font-size: 8px;
+        font-weight :800;
         text-align:center;
         color: rgba(77,85,93,1)
+        .icon
+          order :0;
+          width: 2.5em;
+          height: 2.5em;
+          vertical-align: -0.15em;
+        .icon-font
+          order :1;
+          margin-top :-0.5em;
         &.router-link-active
-          color:rgba(155,150,140,0.8)
+          color:rgba(230,60,230,1);
+          background:rgba(233,233,233,0.6);
+          border-radius :50%;
 </style>
--- travel/src/components/volunteerPost/volunteerPost.vue
+++ travel/src/components/volunteerPost/volunteerPost.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="volunteer">
       义工招募
 
   </div>
@@ -9,6 +9,8 @@
   export default {}
 </script>
 
-<style lang="stylus" rel='stylesheet/stylus'>
 
+<style lang="stylus" rel='stylesheet/stylus'>
+.volunteer
+  background-image:url("../../../static/images/nav-backgroung.jpg")
 </style>
