7230bfc2b3944522c868c76399903409fe47319c
ronny <793976905@qq.com>
2017-11-29 19:35:39 +0800

    fix bug
    fix bug

--- travel/src/main.js
+++ travel/src/main.js
@@ -2,14 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
+import Router from 'vue-router'
 import router from './router'
 import store from './store'
 import './common/stylus/index.styl'
 /* eslint no-multiple-empty-lines: "error" */
 Vue.config.productionTip = false
 
-Vue.use(axios)
+Vue.use(Router)
 
 /* eslint-disable no-new */
  new Vue({
